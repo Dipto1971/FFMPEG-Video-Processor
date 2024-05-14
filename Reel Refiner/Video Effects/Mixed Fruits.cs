e9f0a5d5da2b2e1db2e55312aee0840ccd033003
Dipto1971 <dipto.haq771@gmail.com>
2024-05-14 23:56:48 +0600

    implementing Mixed Fruits Logic.

--- Reel Refiner/Video Effects/Mixed Fruits.cs
+++ Reel Refiner/Video Effects/Mixed Fruits.cs
@@ -5,59 +5,71 @@
         public Mixed_Fruits(string inputVideo, string outputVideo, double duration, int height, int width)
         {
             double firstSlowDuration = duration * 0.25;
-            double secondSlowStart = firstSlowDuration * 2;
+
+            double secondSlowStart = firstSlowDuration;
             double secondSlowEnd = secondSlowStart + 2;
+
+            double thirdSlowStart = secondSlowEnd;
+            double thirdslowEnd = thirdSlowStart + 2;
+
+
+            double fourthslowEnd = thirdslowEnd + 2;
 
             string command =
                 $"[0:v] trim=start={0}:end={firstSlowDuration},  setpts=2*(PTS-STARTPTS) [slow1];" +
 
-                $"color=black:{width}x{height} [base];" +
-                $"[0:v] trim=start={secondSlowStart}:end={secondSlowEnd}, setpts=2*(PTS-STARTPTS), scale={width / 3 - 2}:{height / 2 - 2} [upperleft];" +
-                $"[0:v] trim=start={secondSlowStart}:end={secondSlowEnd}, setpts=2*(PTS-STARTPTS), scale={width / 3}:{height / 2 - 1} [upperright];" +
-                $"[0:v] trim=start={secondSlowStart}:end={secondSlowEnd}, setpts=2*(PTS-STARTPTS), crop={width / 3 - 1}:{height}:{width / 3 - 1}:{0} [Middle];" +
-                $"[0:v] trim=start={secondSlowStart}:end={secondSlowEnd}, setpts=2*(PTS-STARTPTS), scale={width / 3 - 2}:{height / 2 - 2} [lowerleft];" +
-                $"[0:v] trim=start={secondSlowStart}:end={secondSlowEnd}, setpts=2*(PTS-STARTPTS), scale={width / 3}:{height / 2 - 1} [lowerright];" +
 
-                $"[base][Middle] overlay=shortest=1:x={width / 3}:y={0} [tmp1];" +
+                $"color=black:{width}x{height} [collage_1_base];" +
+                $"[0:v] trim=start={secondSlowStart}:end={secondSlowEnd}, setpts=2*(PTS-STARTPTS), scale={width / 3 - 2}:{height / 2 - 2}, lutrgb=b=0:r=0 [upperleft];" +
+                $"[0:v] trim=start={secondSlowStart}:end={secondSlowEnd}, setpts=2*(PTS-STARTPTS), scale={width / 3}:{height / 2 - 1}, lutrgb=b=0 [upperright];" +
+                $"[0:v] trim=start={secondSlowStart}:end={secondSlowEnd}, setpts=2*(PTS-STARTPTS), crop={width / 3 - 1}:{height}:{width / 3 - 1}:{0} [Middle];" +
+                $"[0:v] trim=start={secondSlowStart}:end={secondSlowEnd}, setpts=2*(PTS-STARTPTS), scale={width / 3 - 2}:{height / 2 - 2}, lutrgb=g=152:b=186 [lowerleft];" +
+                $"[0:v] trim=start={secondSlowStart}:end={secondSlowEnd}, setpts=2*(PTS-STARTPTS), scale={width / 3}:{height / 2 - 1}, lutrgb=g=0:b=0  [lowerright];" +
+
+                $"[collage_1_base][Middle] overlay=shortest=1:x={width / 3}:y={0} [tmp1];" +
                 $"[tmp1][upperright] overlay=shortest=1:x={width / 3 * 2}:y={0} [tmp2];" +
                 $"[tmp2][upperleft] overlay=shortest=1:x={1}:y={1} [tmp3];" +
                 $"[tmp3][lowerleft] overlay=shortest=1:x={0}:y={height / 2} [tmp4];" +
-                $"[tmp4][lowerright] overlay=shortest=1:x={width / 3 * 2}:y={height / 2} [collage];" +
-
-                $"[slow1][collage] concat=n=2:v=1:a=0 [out]";
+                $"[tmp4][lowerright] overlay=shortest=1:x={width / 3 * 2}:y={height / 2} [collage_1];" +
 
 
+                $"[0:v]trim=start={thirdSlowStart}:end={thirdslowEnd},setpts=2*(PTS-STARTPTS) [collage_2_base];" +
+                $"[0:v]trim=start={thirdSlowStart + 0.1},setpts=2*(PTS-STARTPTS+.1/TB),scale={width - 60}:{height - 60}[v0scaled];" +
+                $"[0:v]trim=start={thirdSlowStart + 0.2},setpts=2*(PTS-STARTPTS+.2/TB),scale={width - 100}:{height - 100}[v1scaled];" +
+                $"[0:v]trim=start={thirdSlowStart + 0.3},setpts=2*(PTS-STARTPTS+.3/TB),scale={width - 140}:{height - 140}[v2scaled];" +
+                $"[0:v]trim=start={thirdSlowStart + 0.4},setpts=2*(PTS-STARTPTS+.4/TB),scale={width - 180}:{height - 180}[v3scaled];" +
+                $"[0:v]trim=start={thirdSlowStart + 0.5},setpts=2*(PTS-STARTPTS+.5/TB),scale={width - 220}:{height - 220}[v4scaled];" +
+                $"[0:v]trim=start={thirdSlowStart + 0.6},setpts=2*(PTS-STARTPTS+.6/TB),scale={width - 260}:{height - 260}[v5scaled];" +
 
-                string rainbowCollage =
-
-                $"[base][Middle] overlay=shortest=1:x=240:y=0 [tmp1]; " +
-                $"[tmp1][upperright] overlay=shortest=1:x=480:y=0 [tmp2]; " +
-                $"[tmp2][upperleft] overlay=shortest=1:x=1:y=1 [tmp3]; " +
-                $"[tmp3][lowerleft] overlay=shortest=1:x=0:y=640 [tmp4]; " +
-                $"[tmp4][lowerright] overlay=shortest=1:x=480:y=640 [temp5]; " +
-                $"[slow1][normal1] xfade=transition=fadewhite:duration=0.3:offset=11.7 [flashOut]; " +
-                $"[flashOut][temp5] concat=n=2:v=1:a=0 [out] " +
-                $"[tmp4][lowerright] overlay=shortest=1:x=480:y=640" +
-                //Collage 1 end
-                //Collage 2 start
-                $"[0:v]setpts=PTS-STARTPTS[base];" +
-                $"[0:v]trim=start=0.2,setpts=PTS-STARTPTS+.2/TB,crop=680:1240:40:40[v0scaled];" +
-                $"[0:v]trim=start=0.4,setpts=PTS-STARTPTS+.4/TB,crop=640:1200:80:80[v1scaled];" +
-                $"[0:v]trim=start=0.6,setpts=PTS-STARTPTS+.6/TB,crop=600:1160:120:120[v2scaled];" +
-                $"[0:v]trim=start=0.8,setpts=PTS-STARTPTS+.8/TB,crop=560:1120:160:160[v3scaled];" +
-                $"[0:v]trim=start=1.0,setpts=PTS-STARTPTS+1.0/TB,crop=520:1080:180:180[v4scaled];" +
-
-                $"[base][v0scaled]overlay=shortest=1:x=20:y=20 [v0overlayed];" +
-                $"[v0overlayed][v1scaled]overlay=shortest=1:x=40:y=40 [v1overlayed];" +
-                $"[v1overlayed][v2scaled] overlay=shortest=1:x=60:y=60 [v2overlayed];" +
-                $"[v2overlayed][v3scaled] overlay=shortest=1:x=80:y=80 [v3overlayed];" +
-                $"[v3overlayed][v4scaled] overlay=shortest=1:x=100:y=100" +
-                //Collage 2 end
-                $"" +
-                $"";
+                $"[collage_2_base][v0scaled]overlay=shortest=1:x=30:y=30 [v0overlayed];" +
+                $"[v0overlayed][v1scaled]overlay=shortest=1:x=50:y=50 [v1overlayed];" +
+                $"[v1overlayed][v2scaled] overlay=shortest=1:x=70:y=70 [v2overlayed];" +
+                $"[v2overlayed][v3scaled] overlay=shortest=1:x=90:y=90 [v3overlayed];" +
+                $"[v3overlayed][v4scaled] overlay=shortest=1:x=110:y=110 [v4overlayed];" +
+                $"[v4overlayed][v5scaled] overlay=shortest=1:x=130:y=130 [collage_2];" +
 
 
-            string input = $"-i {inputVideo} -filter_complex \"{command}\" -map \"[out]\" -c:a copy -y {outputVideo}";
+
+                $"[0:v] split=5 [0][1][2][3][4];" +
+                $"[0] trim=duration= {thirdslowEnd},setpts=(PTS-STARTPTS), tpad=stop_mode=clone:stop_duration=1000000 [0trimmed];" +
+                $"[1] trim=duration= {thirdslowEnd+.3},setpts=(PTS-STARTPTS), tpad=stop_mode=clone:stop_duration=1000000 [1trimmed];" +
+                $"[2] trim=duration= {thirdslowEnd+.6},setpts=(PTS-STARTPTS), tpad=stop_mode=clone:stop_duration=1000000 [2trimmed];" +
+                $"[3] trim=duration= {thirdslowEnd+.9},setpts=(PTS-STARTPTS), tpad=stop_mode=clone:stop_duration=1000000 [3trimmed];" +
+                $"[4] trim=duration= {thirdslowEnd+ 1.2},setpts=(PTS-STARTPTS), tpad=stop_mode=clone:stop_duration=1000000 [4trimmed];" +
+
+                $"[0:v] trim=start={0}:end={thirdslowEnd+1.2},reverse,setpts=(PTS-STARTPTS) [5reversed];" +
+
+                $"[0trimmed][1trimmed]xfade=transition=wiperight:duration=1:offset={0}[f0];" +
+                $"[f0][2trimmed]xfade=transition=wiperight:duration=1:offset={0.3}[f1];" +
+                $"[f1][3trimmed]xfade=transition=wiperight:duration=1:offset={0.6}[f2];" +
+                $"[f2][4trimmed]xfade=transition=wiperight:duration=1:offset={0.9}[f3];" +
+                $"[f3][5reversed] xfade=transition=wipeleft:duration=1:offset={1.9}[effect_1];" +
+
+                $"[slow1][collage_1][collage_2][effect_1] concat=n=4:v=1:a=0 [out]";
+
+
+
+            string input = $"-i {inputVideo} -filter_complex \"{command}\" -map \"[out]\" -vcodec libx264 -c:a copy -y {outputVideo}";
 
             Utils.Utils.ExecuteFFmpegCommand(input);
         }
